2d069c0ddb7af23c0f03f12a7f52dda2f68d9993
JemarJones <jemarkjones@gmail.com>
2016-01-02 16:28:04 -0500

    Updated resume

--- web/viewer.js
+++ web/viewer.js
@@ -27,7 +27,7 @@
 
 'use strict';
 
-var DEFAULT_URL = '../assets/Jemar%20Jones%20New%20Resume%202016.pdf';
+var DEFAULT_URL = '../assets/Jemar%20Jones%20Resume%202016.pdf';
 var DEFAULT_SCALE_DELTA = 1.1;
 var MIN_SCALE = 0.25;
 var MAX_SCALE = 10.0;
