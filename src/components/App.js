eacf3282d1a25e27319e36f7bd90055706886166
Jemar Jones <jemarkjones@gmail.com>
2018-08-14 23:56:09 -0400

    Updating boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-
 import '../styles/App.css';
 
-const App = () => (
-  <div>
-    Hello World!
-  </div>
-);
+const App = () => <div>Hello World!</div>;
+console.log(8);
 
 export default App;
