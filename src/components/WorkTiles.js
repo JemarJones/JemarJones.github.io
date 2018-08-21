46acd5825af4cf60485ce07f2dd270786a2bdfd8
Jemar Jones <jemarkjones@gmail.com>
2018-08-21 00:48:33 -0400

    Tweaking animations and styles for the tiles

--- src/components/WorkTiles.js
+++ src/components/WorkTiles.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { wrapGrid } from 'animate-css-grid';
 
-const WorkTiles = ({ workList }) => {
-  return (
-    <div className="work-tiles">
-      {workList.map(item => (
-        <div className="work-tiles__item" key={item.name}>
-          {item.name}
-        </div>
-      ))}
-    </div>
-  );
-};
+class WorkTiles extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.tileGrid = React.createRef();
+  }
+
+  componentDidMount() {
+    wrapGrid(this.tileGrid.current);
+  }
+
+  handleTileSelect = (item) => {
+    console.error('Implement work tile select');
+  }
+
+  handleKeyDown = (item, e) => {
+    if (e.keyCode === 13) {
+      this.handleTileSelect(item);
+    }
+  }
+
+  render() {
+    const { workList } = this.props;
+    return (
+      <div
+        className="work-tiles"
+        ref={this.tileGrid}>
+        {workList.map(item => (
+          <div
+            className="work-tiles__item"
+            key={item.name}
+            tabIndex="0"
+            onKeyDown={this.handleKeyDown.bind(this, item)}
+            onClick={this.handleTileSelect}>
+            {item.name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 WorkTiles.propTypes = {
   workList: PropTypes.array,
