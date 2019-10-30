f23141dc7c8414f41f986d41d13ac7be9b852b8b
Jemar Jones <jemarkjones@gmail.com>
2019-10-29 23:52:33 -0400

    Remove console.log, add some eslint rules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export const format = (string: string): string => {
-  console.log(string);
   return string
     .replace(/\n\n/g, '<br><br>') // Replace double newline with a double br.
     .replace(/  +/g, ''); // Replace multiwhitespace with single.
