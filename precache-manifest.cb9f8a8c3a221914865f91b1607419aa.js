8f1edc81f6938bebffbfe5c49ccb2a920a2f1acb
Jemar Jones <jemarkjones@gmail.com>
2019-12-17 00:36:11 -0500

    Updates

---
+++ precache-manifest.cb9f8a8c3a221914865f91b1607419aa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98447f04ec4a1062ffec3ec051090f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f27a36e99863c7d82b9",
+    "url": "/static/css/main.15a842b2.chunk.css"
+  },
+  {
+    "revision": "1341cd537df5d3955c11",
+    "url": "/static/js/2.c071d87e.chunk.js"
+  },
+  {
+    "revision": "dd65fa3cbe6ade16f81900d990817c3f",
+    "url": "/static/js/2.c071d87e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f27a36e99863c7d82b9",
+    "url": "/static/js/main.efa7b4af.chunk.js"
+  },
+  {
+    "revision": "bdd694767d1bdc4ad698",
+    "url": "/static/js/runtime-main.443f3000.js"
+  },
+  {
+    "revision": "9176d0e71fd149108c003deef756f356",
+    "url": "/static/media/akira_banner.9176d0e7.svg"
+  },
+  {
+    "revision": "ce7dc9eeba281144c3792bede6e99312",
+    "url": "/static/media/flash_learn_banner.ce7dc9ee.svg"
+  },
+  {
+    "revision": "d108be84333c7b3f466d15c7f1413727",
+    "url": "/static/media/font_me_banner.d108be84.svg"
+  },
+  {
+    "revision": "bb62bc529fde4cacb9f5ecffe1e9e263",
+    "url": "/static/media/jkjones_dot_me_banner.bb62bc52.png"
+  },
+  {
+    "revision": "7744064e68dc901bcc34727f5c75f964",
+    "url": "/static/media/konrad_banner.7744064e.svg"
+  },
+  {
+    "revision": "64c00f2302a4af4d943da7952ac549b5",
+    "url": "/static/media/live_labs_banner.64c00f23.png"
+  },
+  {
+    "revision": "d401f83ba1fd6b9bb14ab2292039d07b",
+    "url": "/static/media/maccoin_banner.d401f83b.svg"
+  },
+  {
+    "revision": "582d32ddd96174519c1a0bd905ce7616",
+    "url": "/static/media/survival_of_the_fittest_banner.582d32dd.svg"
+  },
+  {
+    "revision": "4842df3613978a59eb3501b6072b97aa",
+    "url": "/static/media/webtunes_banner.4842df36.svg"
+  },
+  {
+    "revision": "4ede50d664dd4d0baabd25aba6c04e9d",
+    "url": "/static/media/yaac.4ede50d6.png"
+  }
+]);
