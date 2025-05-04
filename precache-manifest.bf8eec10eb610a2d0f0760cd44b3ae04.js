f21d6f49da7279237278d8231e0db817e133dc2f
Jemar Jones <jemarkjones@gmail.com>
2025-05-04 14:53:56 -0400

    Updates to build for deploying 209b8be7 to github pages

---
+++ precache-manifest.bf8eec10eb610a2d0f0760cd44b3ae04.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec38ce63aab0b8038bbd2c64467ed0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0a98cbc056e4834e0af",
+    "url": "/static/css/main.2232fabe.chunk.css"
+  },
+  {
+    "revision": "c7130a930cf1807bf6c4",
+    "url": "/static/js/2.f6d984fa.chunk.js"
+  },
+  {
+    "revision": "dd65fa3cbe6ade16f81900d990817c3f",
+    "url": "/static/js/2.f6d984fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0a98cbc056e4834e0af",
+    "url": "/static/js/main.4816f379.chunk.js"
+  },
+  {
+    "revision": "bdd694767d1bdc4ad698",
+    "url": "/static/js/runtime-main.443f3000.js"
+  },
+  {
+    "revision": "9176d0e71fd149108c003deef756f356",
+    "url": "/static/media/akira_banner.9176d0e7.svg"
+  },
+  {
+    "revision": "ce7dc9eeba281144c3792bede6e99312",
+    "url": "/static/media/flash_learn_banner.ce7dc9ee.svg"
+  },
+  {
+    "revision": "d108be84333c7b3f466d15c7f1413727",
+    "url": "/static/media/font_me_banner.d108be84.svg"
+  },
+  {
+    "revision": "bb62bc529fde4cacb9f5ecffe1e9e263",
+    "url": "/static/media/jkjones_dot_me_banner.bb62bc52.png"
+  },
+  {
+    "revision": "7744064e68dc901bcc34727f5c75f964",
+    "url": "/static/media/konrad_banner.7744064e.svg"
+  },
+  {
+    "revision": "64c00f2302a4af4d943da7952ac549b5",
+    "url": "/static/media/live_labs_banner.64c00f23.png"
+  },
+  {
+    "revision": "2f941b8608eeec9be20e22d9bafbb898",
+    "url": "/static/media/maccoin_banner.2f941b86.svg"
+  },
+  {
+    "revision": "2a8e929d64394c8d630de98e5bf62342",
+    "url": "/static/media/moty_banner.2a8e929d.png"
+  },
+  {
+    "revision": "72d91d40834e1d196706705ff5d9c6b0",
+    "url": "/static/media/square_banner.72d91d40.svg"
+  },
+  {
+    "revision": "582d32ddd96174519c1a0bd905ce7616",
+    "url": "/static/media/survival_of_the_fittest_banner.582d32dd.svg"
+  },
+  {
+    "revision": "4842df3613978a59eb3501b6072b97aa",
+    "url": "/static/media/webtunes_banner.4842df36.svg"
+  },
+  {
+    "revision": "4ede50d664dd4d0baabd25aba6c04e9d",
+    "url": "/static/media/yaac.4ede50d6.png"
+  }
+]);
